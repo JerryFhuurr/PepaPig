2b476e3cee04f187ec9a44d33020ca9502840352
Jerry Cao <79422097+JerryFhuurr@users.noreply.github.com>
2022-03-10 19:21:21 +0100

    finish whole app

--- app/src/main/java/com/and/pepapig/MapsFragment.java
+++ app/src/main/java/com/and/pepapig/MapsFragment.java
@@ -1,10 +1,6 @@
 package com.and.pepapig;
 
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +8,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.baidu.location.BDAbstractLocationListener;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
@@ -21,9 +19,7 @@ import com.baidu.mapapi.CoordType;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapStatus;
-import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
-import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.TextureMapView;
---
+++ app/src/main/java/com/and/pepapig/ui/login/LoggedInUserView.java
@@ -0,0 +1,17 @@
+package com.and.pepapig.ui.login;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
